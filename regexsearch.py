976f4380f6cde0d5cf2f44966053c2eefab7630c
JerronMcG <jerron.mcginnis@gmail.com>
2019-09-01 20:42:53 -0400

    Update regexsearch.py

--- regexsearch.py
+++ regexsearch.py
@@ -3,11 +3,11 @@ import re
 
 file_open = open(filename,'r')
 try:
-	regex_in = raw_input("Enter the regular expression")
+	regex_in = raw_input("Enter regular expression")
 	match = re.search(regex_in,filename)
 	if match!=None:
 		match_all = re.findall(regex_in,filename)
 		print "Result:" + str(match_all)
 except IOError as e:
-	print ("File requested doesn't exist. Try again!")
+	print ("File not found")
 file_open.close()
